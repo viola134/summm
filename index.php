00d177e6cb47c83320b048a81a0538a4e5b0dfc5
viola134 <113242400+viola134@users.noreply.github.com>
2022-09-25 19:19:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-
 function even($var)
 {
-    return !($var & 1);
+    //return !($var & 1);
+    return $var % 2 == 0;
 }
 $array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-array_filter($array, "even");
-array_sum(array_filter($array, "even"));
-print_r(array_sum(array_filter($array, "even")));
+$arr_f = array_filter($array, "even");
+$arr_fsum = array_sum($arr_f );
+print_r($arr_fsum);
